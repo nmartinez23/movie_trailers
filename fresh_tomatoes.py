1d9281aabbbccdebfafedcaf97e2d7ff779721f5
Nick Martinez <23nicholasmartinez@gmail.com>
2017-03-10 15:23:03 -0800

    Add function for movie content tile

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -118,3 +118,33 @@ main_page_content = '''
   </body>
 </html>
 '''
+
+
+# A single movie entry html template
+movie_tile_content = '''
+<div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" width="220" height="342">
+    <h2>{movie_title}</h2>
+</div>
+'''
+
+
+def create_movie_tiles_content(movies):
+    # The HTML content for this section of the page
+    content = ''
+    for movie in movies:
+        # Extract the youtube ID from the url
+        youtube_id_match = re.search(
+            r'(?<=v=)[^&#]+', movie.trailer)
+        youtube_id_match = youtube_id_match or re.search(
+            r'(?<=be/)[^&#]+', movie.trailer)
+        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                              else None)
+
+        # Append the tile for the movie with its content filled in
+        content += movie_tile_content.format(
+            movie_title=movie.title,
+            poster_image_url=movie.poster,
+            trailer_youtube_id=trailer_youtube_id
+        )
+    return content
